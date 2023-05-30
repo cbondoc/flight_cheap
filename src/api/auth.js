771fdd7d8c49016d313f8954f4642cf051894dd2
Cary <cbondoc.it@gmail.com>
2023-05-31 07:43:33 +0800

    auth.js prettier

--- src/api/auth.js
+++ src/api/auth.js
@@ -16,29 +16,29 @@ export async function get_iata() {
 }
 
 export async function get_airline_code() {
-    return await api().get("/airline_code/");
+  return await api().get("/airline_code/");
 }
 
 export async function get_search_flights(data) {
-  return await api().post("/search_flights/",data);
+  return await api().post("/search_flights/single", data);
 }
 
 export async function subscribe_sms(data) {
-  return await api().post("/send_sms/",data);
+  return await api().post("/send_sms/", data);
 }
 
 export async function subscribe_email(data) {
-  return await api().post("/send_email/",data);
+  return await api().post("/send_email/", data);
 }
 
 export async function post_booking_users(data) {
-  return await api().post("/booking/users/",data);
+  return await api().post("/booking/users/", data);
 }
 
 export async function post_booking_payment(data) {
-  return await api().post("/booking/payment/",data);
+  return await api().post("/booking/payment/", data);
 }
 
 export async function post_booking_final(data) {
-  return await api().post("/booking/final/",data);
+  return await api().post("/booking/final/", data);
 }
