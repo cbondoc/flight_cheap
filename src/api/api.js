e2a22430782036592e7f69c06e378cb349f63a27
Cary <cbondoc.it@gmail.com>
2023-06-10 07:36:25 +0800

    updated the nedpoint of backend

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const list_except_path_auth = ["/login", "/healthz"];
 export default function handler() {
   const instance = axios.create({
-    baseURL: "http://flightcheap.store",
+    baseURL: "https://api.flightcheap.store",
     withCredentials: true,
   });
   // Add a request interceptor
