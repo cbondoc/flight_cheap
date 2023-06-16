2168acbe1c894b2836bcf39b56269b7679d37f12
Cary <cbondoc.it@gmail.com>
2023-06-16 19:21:10 +0800

    added error notification modal

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -18,6 +18,7 @@ import { DateRangePicker } from "react-date-range";
 import Modal from "./Modal";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
+import ErrorModal from "@/components/ErrorModal";
 
 import {
   get_iata,
@@ -33,6 +34,17 @@ function SearchForm(props) {
   const [fromValue, setFromValue] = useState(null);
   const [toValue, setToValue] = useState(null);
   const [trip, setTrip] = React.useState(10);
+
+  // Error Modal Trigger
+  const [openModal, setOpenModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
 
   // Added filter options to include top level category in search result
   const filterOptions = (options, state) => {
@@ -81,15 +93,27 @@ function SearchForm(props) {
     },
   ]);
 
+  // console.log("ERROR");
+  //       if (error.response && error.response.status === 403) {
+  //         setErrorMessage("Incorrect username / password");
+  //         handleOpenModal();
+  //       } else {
+  //         // Handle other types of errors
+  //         setErrorMessage("Error on API endpoint");
+  //         handleOpenModal();
+  //       }
+
   const handleButtonClick = async () => {
     if (!fromValue || !fromValue.IATA_CODE) {
       // handle error when textbox is empty
-      console.log("From textbox is empty");
+      setErrorMessage("Please enter the departure place in the 'From' field.");
+      handleOpenModal();
       return;
     }
     if (!toValue || !toValue.IATA_CODE) {
       // handle error when textbox is empty
-      console.log("To textbox is empty");
+      setErrorMessage("Please enter the arrival place in the 'To' field.");
+      handleOpenModal();
       return;
     }
     console.log("From: " + JSON.stringify(fromValue.IATA_CODE));
@@ -215,165 +239,172 @@ function SearchForm(props) {
   };
 
   return (
-    <Card
-      sx={{
-        maxWidth: "80%",
-        margin: "50px auto",
-        marginTop: -90,
-        backgroundColor: "white",
-      }}
-    >
-      <Box component="section" sx={{ display: "flex", overflow: "hidden" }}>
-        <Container
-          sx={{
-            mb: 5,
-            position: "relative",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            backgroundColor: "rgba(255, 255, 255, 0.9)",
-            borderRadius: 5,
-            padding: 5,
-          }}
-        >
-          <Grid container spacing={2} sx={{ marginTop: 3, mb: 4 }}>
-            <Grid
-              item
-              xs={6}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                mb: 2,
-              }}
-            >
-              {/* Booking Options */}
-              <FormControl required fullWidth>
-                <InputLabel id="demo-simple-select-autowidth-label">
-                  Booking Options
-                </InputLabel>
-                <Select
-                  value={trip}
-                  onChange={handleChange}
-                  autoWidth
-                  label="Trip"
+    <>
+      <Card
+        sx={{
+          maxWidth: "80%",
+          margin: "50px auto",
+          marginTop: -90,
+          backgroundColor: "white",
+        }}
+      >
+        <Box component="section" sx={{ display: "flex", overflow: "hidden" }}>
+          <Container
+            sx={{
+              mb: 5,
+              position: "relative",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              backgroundColor: "rgba(255, 255, 255, 0.9)",
+              borderRadius: 5,
+              padding: 5,
+            }}
+          >
+            <Grid container spacing={2} sx={{ marginTop: 3, mb: 4 }}>
+              <Grid
+                item
+                xs={6}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  mb: 2,
+                }}
+              >
+                {/* Booking Options */}
+                <FormControl required fullWidth>
+                  <InputLabel id="demo-simple-select-autowidth-label">
+                    Booking Options
+                  </InputLabel>
+                  <Select
+                    value={trip}
+                    onChange={handleChange}
+                    autoWidth
+                    label="Trip"
+                    variant="filled"
+                    color="white"
+                  >
+                    <MenuItem value={21}>No Return</MenuItem>
+                    <MenuItem value={10}>Multi-city</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid
+                item
+                xs={6}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  mb: 2,
+                }}
+              >
+                <FormControl fullWidth>
+                  {/* Notification modal */}
+                  <Modal />
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={3}>
+                {/* From and To */}
+                <Autocomplete
+                  options={iataDataFrom.sort(
+                    (a, b) => -b.Country.localeCompare(a.Country)
+                  )}
+                  groupBy={(option) => option.Country}
+                  getOptionLabel={(option) => option.CITY}
+                  onChange={(event, newValue) => {
+                    setFromValue(newValue);
+                    const fromData = iataDataFrom.filter(
+                      (item) => item?.IATA_CODE !== newValue?.IATA_CODE
+                    );
+                    setIataDataTo(fromData);
+                  }}
+                  filterOptions={filterOptions}
+                  renderInput={(params) => (
+                    <TextField sx={{ width: 1 }} {...params} label="From" />
+                  )}
+                />
+                {/*  Number of Adults */}
+                <TextField
+                  id="filled-number"
+                  label="Adults"
+                  type="number"
+                  defaultValue={1}
+                  sx={{ width: 1, mt: 5 }}
                   variant="filled"
-                  color="white"
-                >
-                  <MenuItem value={21}>No Return</MenuItem>
-                  <MenuItem value={10}>Multi-city</MenuItem>
-                </Select>
-              </FormControl>
+                />
+                {/* Number of children */}
+                <TextField
+                  id="filled-number"
+                  label="Children"
+                  type="number"
+                  defaultValue={0}
+                  sx={{ width: 1, mt: 5 }}
+                />
+              </Grid>
+              <Grid item xs={3}>
+                {/* Cabin Class */}
+                <Autocomplete
+                  options={iataDataTo.sort(
+                    (a, b) => -b.Country.localeCompare(a.Country)
+                  )}
+                  groupBy={(option) => option.Country}
+                  getOptionLabel={(option) => option.CITY}
+                  onChange={(event, newValue) => {
+                    setToValue(newValue);
+                  }}
+                  filterOptions={filterOptions}
+                  renderInput={(params) => (
+                    <TextField sx={{ width: 1 }} {...params} label="To" />
+                  )}
+                />
+                <TextField
+                  id="filled-number"
+                  label="Infant"
+                  defaultValue={0}
+                  type="number"
+                  sx={{ width: 1, mt: 5 }}
+                />
+                <Autocomplete
+                  disablePortal
+                  id="combo-box-demo"
+                  options={cabinClass}
+                  defaultValue={"Economy"}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Cabin Class" />
+                  )}
+                  sx={{ width: 1, mt: 5 }}
+                />
+              </Grid>
+              {/* Date Range Picker */}
+              <Grid item xs={6}>
+                <DateRangePicker
+                  minDate={new Date()}
+                  ranges={dateRange}
+                  onChange={(ranges) => setDateRange([ranges.selection])}
+                />
+              </Grid>
             </Grid>
-            <Grid
-              item
-              xs={6}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                mb: 2,
-              }}
-            >
-              <FormControl fullWidth>
-                {/* Notification modal */}
-                <Modal />
-              </FormControl>
-            </Grid>
-          </Grid>
-          <Grid container spacing={2}>
-            <Grid item xs={3}>
-              {/* From and To */}
-              <Autocomplete
-                options={iataDataFrom.sort(
-                  (a, b) => -b.Country.localeCompare(a.Country)
-                )}
-                groupBy={(option) => option.Country}
-                getOptionLabel={(option) => option.CITY}
-                onChange={(event, newValue) => {
-                  setFromValue(newValue);
-                  const fromData = iataDataFrom.filter(
-                    (item) => item?.IATA_CODE !== newValue?.IATA_CODE
-                  );
-                  setIataDataTo(fromData);
-                }}
-                filterOptions={filterOptions}
-                renderInput={(params) => (
-                  <TextField sx={{ width: 1 }} {...params} label="From" />
-                )}
-              />
-              {/*  Number of Adults */}
-              <TextField
-                id="filled-number"
-                label="Adults"
-                type="number"
-                defaultValue={1}
-                sx={{ width: 1, mt: 5 }}
-                variant="filled"
-              />
-              {/* Number of children */}
-              <TextField
-                id="filled-number"
-                label="Children"
-                type="number"
-                defaultValue={0}
-                sx={{ width: 1, mt: 5 }}
-              />
-            </Grid>
-            <Grid item xs={3}>
-              {/* Cabin Class */}
-              <Autocomplete
-                options={iataDataTo.sort(
-                  (a, b) => -b.Country.localeCompare(a.Country)
-                )}
-                groupBy={(option) => option.Country}
-                getOptionLabel={(option) => option.CITY}
-                onChange={(event, newValue) => {
-                  setToValue(newValue);
-                }}
-                filterOptions={filterOptions}
-                renderInput={(params) => (
-                  <TextField sx={{ width: 1 }} {...params} label="To" />
-                )}
-              />
-              <TextField
-                id="filled-number"
-                label="Infant"
-                defaultValue={0}
-                type="number"
-                sx={{ width: 1, mt: 5 }}
-              />
-              <Autocomplete
-                disablePortal
-                id="combo-box-demo"
-                options={cabinClass}
-                defaultValue={"Economy"}
-                renderInput={(params) => (
-                  <TextField {...params} label="Cabin Class" />
-                )}
-                sx={{ width: 1, mt: 5 }}
-              />
-            </Grid>
-            {/* Date Range Picker */}
-            <Grid item xs={6}>
-              <DateRangePicker
-                minDate={new Date()}
-                ranges={dateRange}
-                onChange={(ranges) => setDateRange([ranges.selection])}
-              />
-            </Grid>
-          </Grid>
 
-          <Grid item xs={3}>
-            <Button
-              variant="contained"
-              onClick={handleButtonClick}
-              sx={{ mt: 4 }}
-            >
-              Search
-            </Button>
-          </Grid>
-        </Container>
-      </Box>
-    </Card>
+            <Grid item xs={3}>
+              <Button
+                variant="contained"
+                onClick={handleButtonClick}
+                sx={{ mt: 4 }}
+              >
+                Search
+              </Button>
+            </Grid>
+          </Container>
+        </Box>
+      </Card>
+      <ErrorModal
+        open={openModal}
+        onClose={handleCloseModal}
+        errorMessage={errorMessage}
+      />
+    </>
   );
 }
 
