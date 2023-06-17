1359fedbb1220c4ee55c1e1a7f867811b47e7725
Cary <cbondoc.it@gmail.com>
2023-06-17 09:58:44 +0800

    try on cors issue

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,5 +1,4 @@
 import api from "./api";
-import { NextResponse } from "next/server";
 
 export async function healthz() {
   return await api().get("/healthz");
@@ -13,37 +12,37 @@ export async function login(username, password) {
 }
 
 export async function get_iata() {
-  return await api().get("/iata_code/");
+  return await api().get("/iata_code");
 }
 
 export async function get_airline_code() {
-  return await api().get("/airline_code/");
+  return await api().get("/airline_code");
 }
 
 export async function get_search_flights_single(data) {
-  return await api().post("/search_flights/single/",data);
+  return await api().post("/search_flights/single", data);
 }
 
 export async function get_search_flights_multiple(data) {
-  return await api().post("/search_flights/multiple/", data);
+  return await api().post("/search_flights/multiple", data);
 }
 
 export async function subscribe_sms(data) {
-  return await api().post("/send_sms/", data);
+  return await api().post("/send_sms", data);
 }
 
 export async function subscribe_email(data) {
-  return await api().post("/send_email/", data);
+  return await api().post("/send_email", data);
 }
 
 export async function post_booking_users(data) {
-  return await api().post("/booking/users/", data);
+  return await api().post("/booking/users", data);
 }
 
 export async function post_booking_payment(data) {
-  return await api().post("/booking/payment/", data);
+  return await api().post("/booking/payment", data);
 }
 
 export async function post_booking_final(data) {
-  return await api().post("/booking/final/", data);
+  return await api().post("/booking/final", data);
 }
