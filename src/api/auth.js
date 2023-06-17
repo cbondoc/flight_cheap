6c082da41886c1219178ccc0417a557398c2ce15
pauly-codev <pauly@codev.com>
2023-06-17 09:50:28 +0800

    Initial bug fixes for 307 error

--- src/api/auth.js
+++ src/api/auth.js
@@ -12,37 +12,37 @@ export async function login(username, password) {
 }
 
 export async function get_iata() {
-  return await api().get("/iata_code/");
+  return await api().get("/iata_code");
 }
 
 export async function get_airline_code() {
-  return await api().get("/airline_code/");
+  return await api().get("/airline_code");
 }
 
 export async function get_search_flights_single(data) {
-  return await api().post("/search_flights/single/",data);
+  return await api().post("/search_flights/single",data);
 }
 
 export async function get_search_flights_multiple(data) {
-  return await api().post("/search_flights/multiple/",data);
+  return await api().post("/search_flights/multiple",data);
 }
 
 export async function subscribe_sms(data) {
-  return await api().post("/send_sms/", data);
+  return await api().post("/send_sms", data);
 }
 
 export async function subscribe_email(data) {
-  return await api().post("/send_email/", data);
+  return await api().post("/send_email", data);
 }
 
 export async function post_booking_users(data) {
-  return await api().post("/booking/users/", data);
+  return await api().post("/booking/users", data);
 }
 
 export async function post_booking_payment(data) {
-  return await api().post("/booking/payment/", data);
+  return await api().post("/booking/payment", data);
 }
 
 export async function post_booking_final(data) {
-  return await api().post("/booking/final/", data);
+  return await api().post("/booking/final", data);
 }
