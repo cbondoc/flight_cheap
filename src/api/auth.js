b153080fb8c09280e6771f8cac2774a887b7a7cd
Cary <cbondoc.it@gmail.com>
2023-06-17 10:06:10 +0800

    try on cors issue # 2 - paul

--- src/api/auth.js
+++ src/api/auth.js
@@ -12,27 +12,27 @@ export async function login(username, password) {
 }
 
 export async function get_iata() {
-  return await api().get("/iata_code");
+  return await api().get("/iata_code/");
 }
 
 export async function get_airline_code() {
-  return await api().get("/airline_code");
+  return await api().get("/airline_code/");
 }
 
 export async function get_search_flights_single(data) {
-  return await api().post("/search_flights/single", data);
+  return await api().post("/search_flights/single",data);
 }
 
 export async function get_search_flights_multiple(data) {
-  return await api().post("/search_flights/multiple", data);
+  return await api().post("/search_flights/multiple",data);
 }
 
 export async function subscribe_sms(data) {
-  return await api().post("/send_sms", data);
+  return await api().post("/send_sms/", data);
 }
 
 export async function subscribe_email(data) {
-  return await api().post("/send_email", data);
+  return await api().post("/send_email/", data);
 }
 
 export async function post_booking_users(data) {
